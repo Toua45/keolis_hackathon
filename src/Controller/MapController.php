6823bdb43ae1b3ca78c7ff9d39fa05c23e1ead1b
wilder <anthonykwanchung@gmail.com>
2019-12-19 14:30:27 +0100

    modif maps et bus en option

--- src/Controller/MapController.php
+++ src/Controller/MapController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Service\BusService;
 use App\Service\MapService;
 use App\Service\ParkingService;
 use Symfony\Component\HttpFoundation\Response;
@@ -13,13 +14,15 @@ class MapController extends AbstractController
      * @Route("/map", name="map")
      * @return Response
      */
-    public function index(MapService $mapService, ParkingService $parkingService): Response
+    public function index(MapService $mapService, ParkingService $parkingService, BusService $busService): Response
     {
         $infoStations = $mapService->getInfoStations();
         $infoParkings = $parkingService->getInfoParking();
+        $infoBus = $busService->getInfoBus();
         return $this->render('map.html.twig', [
             'infoStations' => $infoStations,
             'infoParkings' => $infoParkings,
+            'infoBus' => $infoBus,
         ]);
     }
 }
