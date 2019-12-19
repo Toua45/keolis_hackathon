877f4476c5aa9d7d6515f7a6e6b089e2507ab688
wilder <anthonykwanchung@gmail.com>
2019-12-19 14:33:33 +0100

    delete bus map

--- src/Controller/MapController.php
+++ src/Controller/MapController.php
@@ -2,7 +2,6 @@
 
 namespace App\Controller;
 
-use App\Service\BusService;
 use App\Service\MapService;
 use App\Service\ParkingService;
 use Symfony\Component\HttpFoundation\Response;
@@ -14,15 +13,13 @@ class MapController extends AbstractController
      * @Route("/map", name="map")
      * @return Response
      */
-    public function index(MapService $mapService, ParkingService $parkingService, BusService $busService): Response
+    public function index(MapService $mapService, ParkingService $parkingService): Response
     {
         $infoStations = $mapService->getInfoStations();
         $infoParkings = $parkingService->getInfoParking();
-        $infoBus = $busService->getInfoBus();
         return $this->render('map.html.twig', [
             'infoStations' => $infoStations,
             'infoParkings' => $infoParkings,
-            'infoBus' => $infoBus,
         ]);
     }
 }
