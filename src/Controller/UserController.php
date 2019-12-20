bb1d02fb41abc43b0883ad277603ef8378e70158
Toua VA <toua.va45@gmail.com>
2019-12-20 09:54:20 +0100

    Trip page fini !

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -32,8 +32,8 @@ class UserController extends AbstractController
 //        ceil($user->getXp() / self::LEVEL_COEFF);
         //nb points
 
+        return $this->redirectToRoute('score_index');
 
-        return $this->render ('score/index.html.twig');
     }
 
 }
